45bfd8af9084964aee507e6e762cf3b21208b21d
Rohit suman <165371604+sainin-web@users.noreply.github.com>
2026-08-11 12:50:42 +0530

    first commit

---
+++ wix-consultant-backend/test.js
@@ -0,0 +1,15 @@
+// let a = 10
+// a = 20
+// console.log(a)
+const a = [1, 2, 3, 4, 5, 6, 7]
+
+const test = (q, b) => {
+    console.log(q- b)
+}
+test(10, "6")
+
+
+// for (let b of a) {
+//     console.log(a)
+// }
+
